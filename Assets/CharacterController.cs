4ded00bbe3c4917a2ccf469c6b1b2b7e53adc028
Tobias-Martinsson <59065729+Tobias-Martinsson@users.noreply.github.com>
2021-10-12 10:57:33 +0200

    Initial commit

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    [SerializeField]
+    float moveSpeed = 4f;
+    private Plane plane;
+
+    Vector3 forward, right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = new Plane(Vector3.up, Vector3.zero);
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out var enter))
+        {
+            var hitPoint = ray.GetPoint(enter);
+            var playerPositionOnPlane = plane.ClosestPointOnPlane(transform.position);
+            transform.rotation = Quaternion.LookRotation(hitPoint - playerPositionOnPlane);
+        }
+        if (Input.anyKey)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+        transform.position += rightMovement;
+        transform.position += upMovement;
+    }
+}
