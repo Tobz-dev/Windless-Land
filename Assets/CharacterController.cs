89f3b613f5164767eb06d331a263ee00cd6fcdc7
ra1l0 <agelii.tim@gmail.com>
2021-10-13 11:52:34 +0200

    Fixed diagonal speed

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -7,7 +7,9 @@ public class CharacterController : MonoBehaviour
 
     [SerializeField]
     float moveSpeed = 4f;
+    
     private Plane plane;
+
 
     Vector3 forward, right;
     // Start is called before the first frame update
@@ -40,7 +42,15 @@ public class CharacterController : MonoBehaviour
     {
         Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
         Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
-        transform.position += rightMovement;
-        transform.position += upMovement;
+
+        Vector3 playerMovement = rightMovement + upMovement;
+
+        if (playerMovement.magnitude > moveSpeed * Time.deltaTime)
+        {
+            playerMovement = playerMovement.normalized * moveSpeed * Time.deltaTime;
+        }
+        Debug.Log(playerMovement.magnitude);
+        transform.position += playerMovement;
+        
     }
 }
