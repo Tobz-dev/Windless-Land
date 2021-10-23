e10bee1e8aea44bade6b6af2afc0faa3f1a74fe4
AngryBread-git <67788270+AngryBread-git@users.noreply.github.com>
2021-10-23 14:29:05 +0200

    animations

--- Assets/LukasKleberg/MainMenu/FontChanger.cs
+++ Assets/LukasKleberg/MainMenu/FontChanger.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class FontChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //it can update the children of an inactive object. but not if the inactive object is a child of the parent.
+    //could solve that by having an array. for each of the menus?
     [SerializeField]
-    private GameObject gameObject;
+    private GameObject parentObject;
 
-    public void FontChangeGlobal(Font fontToChangeTo) 
+    //if the text is on UI, it has to be specified on declaration
+    private TextMeshProUGUI[] textMeshProUGUIarray;
+
+    //remembering fonts between play sessions would be nice.
+
+
+    public void FontChange(TMP_FontAsset fontToChangeTo) 
     {
-        /*        hingeJoints = GetComponentsInChildren<HingeJoint>();
 
-        foreach (HingeJoint joint in hingeJoints)
-            joint.useSpring = false;
-            */
+        //find all the TMPs in the children
+        textMeshProUGUIarray = parentObject.GetComponentsInChildren<TextMeshProUGUI>();
+        Debug.Log("in font change" + textMeshProUGUIarray.Length);
+
+        //change the font for each of them. 
+        //cloud probably also change the size as well.
+        foreach (TextMeshProUGUI textMeshProUGUI in textMeshProUGUIarray) 
+        {
+            Debug.Log("in font change, for each loop");
+            textMeshProUGUI.font = fontToChangeTo;
+        }
+
+        //TODO don't change the buttons that showcase the different fonts. or just change them back.
     }
 }
