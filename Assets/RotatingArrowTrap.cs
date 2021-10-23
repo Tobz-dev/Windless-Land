2b627e9802eaa55cafbc12777a64f3f8cbe12e2f
Tobias-Martinsson <59065729+Tobias-Martinsson@users.noreply.github.com>
2021-10-23 15:19:27 +0200

    1

--- Assets/RotatingArrowTrap.cs
+++ Assets/RotatingArrowTrap.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class RotatingArrowTrap : MonoBehaviour
 {
+    private float timeLeft = 0.1f;
+    private float originalTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalTime = timeLeft;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 50 * Time.deltaTime, 0);
-        GetComponent<ArrowScript>().shootArrow();
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            GetComponent<ArrowScript>().shootArrow();
+            timeLeft = originalTime;
+        }
     }
 }
