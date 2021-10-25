15e20b32345d437a18be8bf63a16e24c96195fa5
IlirianZuta <82038180+IlirianZuta@users.noreply.github.com>
2021-10-25 11:33:03 +0200

    as

--- Assets/ScriptsByIlirian/DoorOpenAnim.cs
+++ Assets/ScriptsByIlirian/DoorOpenAnim.cs
@@ -1,22 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorOpenAnim : MonoBehaviour
 {
 
     private Animator anim;
+    public PickUpManager player;
+    private bool doorCanOpen;
+    private bool doorIsOpen;
+    [SerializeField] private Text pressText;
+    [SerializeField] private GameObject panel;
+    private Collider doorCollider;
 
 
-    private void Awake()
+    void Start()
     {
         anim = GetComponent<Animator>();
+        panel.SetActive(false);
+        doorCollider = GetComponent<BoxCollider>();
     }
 
     public void openDoor()
     {
         anim.SetBool("Open", true);
+        player.resetKeyValue();
+        Debug.Log("No longer have key");
+        doorCanOpen = false;
+        doorIsOpen = true;
+        doorCollider.enabled = false;
+        if (!doorCanOpen)
+        {
+            panel.SetActive(false);
+        }
+
+        
     }
+
+    void Update()
+    {
+        if(doorCanOpen && player.getKey() == 1 && Input.GetKeyDown(KeyCode.E)){
+            openDoor();
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            doorCanOpen = true;
+            panel.SetActive(true);
+            checkIfDoorOpens();
+
+            if (doorIsOpen)
+            {
+                panel.SetActive(false);
+            }
+
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            doorCanOpen = false;
+            panel.SetActive(false);
+        }
+    }
+
+    public void checkIfDoorOpens()
+    {
+        if (doorCanOpen && player.getKey() == 1)
+        {
+            pressText.text = "Press E to unlock!";
+        }
+        else
+        {
+            pressText.text = "Door is Locked";
+        }
+    }
+
+    
 
 
 }
--- Assets/ScriptsByIlirian/PickUpManager.cs
+++ Assets/ScriptsByIlirian/PickUpManager.cs
@@ -5,93 +5,152 @@ using UnityEngine.UI;
 
 public class PickUpManager : MonoBehaviour
 {
-    public DoorOpenAnim anim;
+    //public DoorOpenAnim anim;
+    //public LeverAnim leveranim;
+
 
     [SerializeField] private GameObject panel;
-    [SerializeField] private GameObject logPanel;
+    //[SerializeField] private GameObject logPanel;
     private bool pickUpAllowed;
-    private bool insideArea;
-    private bool readLog;
-    [SerializeField] private GameObject key;
+    //private bool insideArea;
+    //private bool readLog;
+    //private bool hasOpenedDoor;
+    //private bool canPushLever;
+    private GameObject key;
     [SerializeField] private Text pressText;
-    [SerializeField] private Text logText;
-    private bool hasKey;
-    //[SerializeField] private GameObject door;
-    
+    //[SerializeField] private Text logText;
+    private int hasKey;
+
+
 
 
     void Start()
     {
-        panel.SetActive(false);
-        hasKey = false;
-        logPanel.SetActive(false);
+        //panel.SetActive(false);
+        hasKey = 0;
+        //logPanel.SetActive(false);
     }
 
     private void Update()
     {
+
         if(pickUpAllowed && Input.GetKeyDown(KeyCode.E))
         {
-            PickUp();
+            key.SetActive(false);
+            panel.SetActive(false);
+            hasKey++;
+            Debug.Log("GOT KEY");
+            pickUpAllowed = false;
         }
 
-        if(insideArea && hasKey && Input.GetKeyDown(KeyCode.E))
+        
+        /*if(pickUpAllowed && !hasKey && Input.GetKeyDown(KeyCode.E))
+        {
+            key.SetActive(false);
+            panel.SetActive(false);
+            hasKey = true;
+            Debug.Log("HAS KEY");
+        }*/
+
+       /* if (insideArea && hasKey == 1 && Input.GetKeyDown(KeyCode.E))
         {
             anim.openDoor();
-            hasKey = false;
+            hasKey = 0;
             Debug.Log("No longer have key");
-           
+            insideArea = false;
+            panel.SetActive(false);
+            hasOpenedDoor = true;
+
+            if (hasOpenedDoor)
+            {
+                panel.SetActive(false);
+            }
+
+
+
+
         }
-        if(readLog && Input.GetKeyDown(KeyCode.E))
+        if (readLog && Input.GetKeyDown(KeyCode.E))
         {
             logPanel.SetActive(true);
 
-            if(logPanel == isActiveAndEnabled)
+            if (logPanel == isActiveAndEnabled)
             {
                 panel.SetActive(false);
             }
         }
+
+        if (canPushLever && Input.GetKeyDown(KeyCode.E))
+        {
+            panel.SetActive(false);
+            leveranim.pullLever();
+        }*/
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Key"))
         {
-            panel.SetActive(true);
+            key = other.gameObject;
             pickUpAllowed = true;
-            pressText.text = "Press E To Pick Up Key";
+            //key.SetActive(false);
+            panel.SetActive(true);
+            pressText.text = "Press E to pick up key!";
+
+            
+            //panel.SetActive(true);
+            //pickUpAllowed = true;
+            //hasKey++;
+            //Debug.Log("I have 1 Key right now");
+            //pressText.text = "Press E To Pick Up Key";
         }
 
-        if (other.gameObject.CompareTag("Door"))
+        /*if (other.gameObject.CompareTag("Door"))
         {
             panel.SetActive(true);
             insideArea = true;
 
-            if (hasKey)
+
+            if (hasKey == 1)
             {
                 pressText.text = "Press E to Open";
             }
             else
             {
-            pressText.text = "You need a key to open!!";
+                pressText.text = "You need a key to open!!";
             }
-            
-        }
-        if (other.gameObject.CompareTag("Logs"))
+
+        }*/
+        /*if (other.gameObject.CompareTag("Logs"))
         {
             panel.SetActive(true);
             pressText.text = "Press E to read Log";
             readLog = true;
         }
+
+        if (other.gameObject.CompareTag("Lever"))
+        {
+            panel.SetActive(true);
+            pressText.text = "press E to pull lever!";
+            canPushLever = true;
+        }*/
     }
 
     private void OnTriggerExit(Collider other)
     {
+
         if (other.gameObject.CompareTag("Key"))
+        {
+            key = other.gameObject;
+            pickUpAllowed = false;
+            panel.SetActive(false);
+        }
+        /*if (other.gameObject.CompareTag("Key"))
         {
             panel.SetActive(false);
             pickUpAllowed = false;
-        }
-        if (other.gameObject.CompareTag("Door"))
+        }*/
+        /*if (other.gameObject.CompareTag("Door"))
         {
             panel.SetActive(false);
             insideArea = false;
@@ -101,22 +160,32 @@ public class PickUpManager : MonoBehaviour
             panel.SetActive(false);
             readLog = false;
         }
+        if (other.gameObject.CompareTag("Lever"))
+        {
+            panel.SetActive(false);
+            canPushLever = false;
+        }*/
     }
 
-    private void PickUp()
-    {
-        key.SetActive(false);
-        panel.SetActive(false);
-        hasKey = true;
-        Debug.Log("HAS KEY");
-    }
 
-    public void closeLogPanel()
+
+    /*public void closeLogPanel()
     {
         logPanel.SetActive(false);
         if (readLog)
         {
             panel.SetActive(true);
         }
+    }*/
+
+    public int getKey()
+    {
+        return hasKey;
     }
+
+    public void resetKeyValue()
+    {
+        hasKey = 0;
+    }
+
 }
