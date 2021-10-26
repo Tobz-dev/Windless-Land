f121f4aec8f6428efbb23fda89a876e6d57c125b
SOTGGON <maoxiehui@outlook.com>
2021-10-26 20:50:53 +0200

    Fallingplatform respawn

--- Assets/Source Material/Xiehui Mao/Scripts/FallingPlatform.cs
+++ Assets/Source Material/Xiehui Mao/Scripts/FallingPlatform.cs
@@ -5,19 +5,67 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
 
-    public float fallDelay = 2.0f;
+    private Rigidbody rbd;
+    private BoxCollider boxcollider;
 
-    void OnCollisionEnter(Collision collidedWithThis)
+    public float fallplat;
+
+    public float fallplaton;
+
+    public bool isFalling = false;
+
+    public Vector3 initialposition;
+
+    private void Awake()
     {
-        if (collidedWithThis.gameObject.tag == "Player")
+        rbd = GetComponent<Rigidbody>();
+        boxcollider = GetComponent<BoxCollider>();
+    }
+
+    private void Start()
+    {
+        initialposition = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(FallAfterDelay());
+            Invoke("Fall", fallplat);
         }
     }
 
-    IEnumerator FallAfterDelay()
+    void Fall()
     {
-        yield return new WaitForSeconds(fallDelay);
-        GetComponent<Rigidbody>().isKinematic = false;
+        rbd.isKinematic = false;
+        boxcollider.isTrigger = true;
+        isFalling = true;
+    }
+
+    void respawn()
+    {
+        StartCoroutine(respawnco());
+    }
+
+    IEnumerator respawnco()
+    {
+        yield return new WaitForSeconds(fallplaton);
+        isFalling = false;
+        rbd.isKinematic = true;
+        boxcollider.isTrigger = false;
+        transform.position = initialposition;
+        rbd.velocity = Vector3.zero;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Death")
+
+        {
+            rbd.isKinematic = true;
+            boxcollider.isTrigger = false;
+            isFalling = false;
+            respawn();
+        }
     }
 }
