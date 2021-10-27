2372110c1a476a7121097e0aefaf4cc8647b994e
AngryBread-git <67788270+AngryBread-git@users.noreply.github.com>
2021-10-27 15:47:40 +0200

    added in resolution changing and fullscreen toggle.

---
+++ Assets/LukasKleberg/MainMenu/ChangeScreenResolution.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeScreenResolution : MonoBehaviour
+{
+
+    private bool inFullscreen;
+    public Image checkBoxImage;
+
+    private Resolution[] resolutionsArray;
+
+    [SerializeField]
+    private TMP_Dropdown resolutionTMPDropdown;
+
+    public void Start()
+    {
+        //gather info about the possible resolutions
+        resolutionsArray = Screen.resolutions;
+
+        //clear the revious options in the dropdown
+        resolutionTMPDropdown.ClearOptions();
+
+        List<string> resolutionOptions = new List<string>();
+        int currentResolutionIndex = 0;
+
+
+        for (int i = 0; i < resolutionsArray.Length; i++) 
+        {
+            string option = resolutionsArray[i].width + " x " + resolutionsArray[i].height;
+            resolutionOptions.Add(option);
+
+            if (resolutionsArray[i].width == Screen.currentResolution.width && 
+                resolutionsArray[i].height == Screen.currentResolution.height) 
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        Debug.Log("ChangeScreenResolution. resolutionOptions count is: " + resolutionOptions.Count);
+        resolutionTMPDropdown.AddOptions(resolutionOptions);
+        resolutionTMPDropdown.value = currentResolutionIndex;
+        resolutionTMPDropdown.RefreshShownValue();
+
+
+
+        //check if the game starts in fullscreen
+        if (Screen.fullScreen == true)
+        {
+            Debug.Log("ChangeScreenResolution. on start set to fullscreen");
+            inFullscreen = true;
+            checkBoxImage.enabled = true;
+        }
+    }
+
+    public void SetResolution(int resolutionIndex) 
+    {
+        Resolution newResolution = resolutionsArray[resolutionIndex];
+        Screen.SetResolution(newResolution.width, newResolution.height, Screen.fullScreen);
+    }
+
+    public void ToggleWindow()
+    {
+
+        if (inFullscreen)
+        {
+            Debug.Log("ChangeScreenResolution. set to windowed");
+            Screen.fullScreen = false;
+            inFullscreen = false;
+        }
+        else
+        {
+            Debug.Log("ChangeScreenResolution. set to fullscreen");
+            Screen.fullScreen = true;
+            inFullscreen = true;
+        }
+    }
+}
