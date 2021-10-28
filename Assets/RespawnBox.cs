bc590a7af8542ae19a1dbf5ac9a6ea97a708dc42
Tobias-Martinsson <59065729+Tobias-Martinsson@users.noreply.github.com>
2021-10-28 11:06:59 +0200

    fixed respawnbox collider

--- Assets/RespawnBox.cs
+++ Assets/RespawnBox.cs
@@ -17,7 +17,7 @@ public class RespawnBox : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.tag == "Player")
         {
