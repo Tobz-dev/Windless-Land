01ce407959112edefa7876ca01a5c21b47697693
PinkPenguinKing <henrik_ruden@hotmail.com>
2021-11-11 19:19:22 +0100

    sdsrrt

--- Assets/Henrik/Prototype/PrototypeCharacterController.cs
+++ Assets/Henrik/Prototype/PrototypeCharacterController.cs
@@ -464,4 +464,25 @@ public class PrototypeCharacterController : MonoBehaviour
 
     }
 
+
+    public GameObject FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
 }
