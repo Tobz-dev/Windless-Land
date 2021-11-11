c28fcc44b087e77b6140d136215fa962cb6ca165
Tobias-Martinsson <59065729+Tobias-Martinsson@users.noreply.github.com>
2021-11-11 10:45:56 +0100

    fixed floor spikes + boss rotation

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -544,7 +544,8 @@ public class CharacterController : MonoBehaviour
     public void Respawn()
     {
         Debug.Log("Player Dead");
-        GetComponent<HealthScript>().regainHealth(100);
+        //GetComponent<HealthScript>().regainHealth(100);
+        GetComponent<HealthScript>().takeDamage(100);
         Dead = FMODUnity.RuntimeManager.CreateInstance("event:/Character/Player/Dead");
         Dead.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         Dead.start();
