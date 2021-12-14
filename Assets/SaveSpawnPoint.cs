09a806729256d885336d1fd78d67a429ec5c63df
Tobias-Martinsson <59065729+Tobias-Martinsson@users.noreply.github.com>
2021-12-14 12:42:56 +0100

    E to interact with checkpoints

--- Assets/SaveSpawnPoint.cs
+++ Assets/SaveSpawnPoint.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SaveSpawnPoint : MonoBehaviour
 {
+    public GameObject panel;
+    public TextMeshProUGUI pressText;
 
-    private void OnTriggerEnter(Collider collision)
+
+    private void Update()
     {
-        GameObject collisionObject = collision.gameObject;
-        if (collisionObject.tag == "Player")
+        if (Input.GetKeyDown(KeyCode.E))
         {
-
             foreach (GameObject go in GameObject.FindGameObjectsWithTag("Enemy"))
             {
-                if(go.name != "Boss")
+                if (go.name != "Boss")
                 {
                     Destroy(go);
                 }
@@ -24,11 +27,27 @@ public class SaveSpawnPoint : MonoBehaviour
             {
 
                 go.GetComponent<EnemyRespawnScript>().RespawnEnemy();
-                
+
             }
-            collisionObject.GetComponent<PlayerHealthScript>().regainHealth(100);
-            collisionObject.GetComponent<PlayerHealthScript>().ResetPotions();
-            collisionObject.GetComponent<CharacterController>().SetRespawnPoint(transform.position);
+            GetComponent<CheckpointVFX>().StartEffect();
+
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<PlayerHealthScript>().regainHealth(100);
+            player.GetComponent<PlayerHealthScript>().ResetPotions();
+            player.GetComponent<CharacterController>().SetRespawnPoint(transform.position);
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        panel.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            panel.SetActive(true);       
         }
     }
 }
