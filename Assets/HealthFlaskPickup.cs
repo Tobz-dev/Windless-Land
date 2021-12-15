9290d28b73f913a857ba3fbd2d916b12995bdb13
Tobias-Martinsson <59065729+Tobias-Martinsson@users.noreply.github.com>
2021-12-15 13:19:20 +0100

    potions

--- Assets/HealthFlaskPickup.cs
+++ Assets/HealthFlaskPickup.cs
@@ -7,13 +7,6 @@ public class HealthFlaskPickup : MonoBehaviour
     //private bool destroy = false;
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "Player")
-        {
-            if (collision.gameObject.GetComponent<PlayerHealthScript>().RegainFlask() == true)
-            {
-                Debug.Log("Destroy health pickup");
-                Destroy(gameObject);
-            }
-        }
+       
     }
 }
