e8c079146e9afb6ad3da766bf43acb869b636447
mirandart <60674005+mirandart@users.noreply.github.com>
2021-12-16 19:59:05 +0100

    log and levers tied to rebindsystem

--- Assets/ScriptsByIlirian/PanelHandler.cs
+++ Assets/ScriptsByIlirian/PanelHandler.cs
@@ -19,70 +19,85 @@ public class PanelHandler : MonoBehaviour
 
     [SerializeField] private GameObject PressE;
 
+    private PlayerInputs inputActions;
 
+    private void Awake()
+    {
+        inputActions = InputManager.inputActions;
+    }
+
+    private void OnEnable()
+    {
+        inputActions.WindlessLand.Enable();
+    }
 
     void Start()
     {
         logPanel.SetActive(false);
         //pm = GetComponent<PauseMenu>();
-        PressE.SetActive(false);        
+        PressE.SetActive(false);
 
     }
     void Update()
     {
-        if (readLog && Input.GetKeyDown(KeyCode.E))
+        if (inputActions.WindlessLand.Interact.triggered/*Input.GetKeyDown(KeyCode.E)*/)
         {
-            
-            logPanel.SetActive(true);
-
-            PageOpen = FMODUnity.RuntimeManager.CreateInstance("event:/Environment/PageOpen");
-            PageOpen.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
-            PageOpen.start();
-            PageOpen.release();
-
-
-
-
-            if (logPanel == isActiveAndEnabled)
+            if (!logPanel.activeSelf)
             {
-              //panel.SetActive(false);
-                PressE.SetActive(false);
+                logPanel.SetActive(true);
+
+                PageOpen = FMODUnity.RuntimeManager.CreateInstance("event:/Environment/PageOpen");
+                PageOpen.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+                PageOpen.start();
+                PageOpen.release();
+
+
+
+
+                if (logPanel == isActiveAndEnabled)
+                {
+                    //panel.SetActive(false);
+                    PressE.SetActive(false);
+                }
+
+
+                switch (logNumber)
+                {
+                    //TODO. have them change if the player is using a controller. 
+                    //or if they have rebound the controls.
+                    case 1:
+                        textmesh.text = "WASD to walk, \n \n  Spacebar to roll, \n \n  Left Mouse Button to attack";
+                        break;
+                    case 2:
+                        textmesh.text = "Q to drink a healing potion";
+                        break;
+                    case 3:
+                        textmesh.text = "Press the Right Mouse Button for a heavy attack. Heavy attacks consume mana";
+                        break;
+                    case 4:
+                        textmesh.text = "Hitting enemies will restore your mana. Another way is to go near a Mana Fountain";
+                        break;
+                    case 5:
+                        textmesh.text = "Enemies respawn when a checkpoint is activated. Be careful.";
+                        break;
+                    default:
+                        textmesh.text = "Default";
+                        break;
+
+
+                }
+            }
+            else
+            {
+                return;
             }
 
-
-            switch (logNumber)
-            {
-                //TODO. have them change if the player is using a controller. 
-                //or if they have rebound the controls.
-                case 1:
-                    textmesh.text = "WASD to walk, \n \n  Spacebar to roll, \n \n  Left Mouse Button to attack";
-                    break;
-                case 2:
-                    textmesh.text = "Q to drink a healing potion";
-                    break;
-                case 3:
-                    textmesh.text = "Press the Right Mouse Button for a heavy attack. Heavy attacks consume mana";
-                    break;
-                case 4:
-                    textmesh.text = "Hitting enemies will restore your mana. Another way is to go near a Mana Fountain";
-                    break;
-                case 5:
-                    textmesh.text = "Enemies respawn when a checkpoint is activated. Be careful.";
-                    break;
-                default:
-                    textmesh.text = "Default";
-                    break;
-
-
-            }
         }
-
-        if (logPanel == isActiveAndEnabled && Input.GetKeyDown(KeyCode.Escape))
+        else if (logPanel == isActiveAndEnabled && Input.anyKeyDown)
         {
-            
             logPanel.SetActive(false);
-
         }
+
     }
 
 
