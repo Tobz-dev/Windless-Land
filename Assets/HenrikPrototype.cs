79fd55d8bf72d3878c8262e1da9274ac2f8fb30f
PinkPenguinKing <henrik_ruden@hotmail.com>
2021-12-16 16:37:12 +0100

    fsdsds

---
+++ Assets/HenrikPrototype.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HenrikPrototype : MonoBehaviour
+{
+    private bool moreDamage = false;
+
+    private bool cooldownIdikator = false;
+
+    private bool autoAim = false;
+
+    [SerializeField]
+    private int addedDamage;
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject playerAttackHitbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void autoAimButton()
+    {
+        if(autoAim == false)
+        {
+            //Debug.Log("Auto Aim On");
+            autoAim = true;
+        }
+        else
+        {
+            //Debug.Log("Auto Aim Off");
+            autoAim = false;
+        }
+    }
+
+    public void cooldownIdikatorButton()
+    {
+        if (cooldownIdikator == false)
+        {
+            //Debug.Log("Auto Aim On");
+            cooldownIdikator = true;
+        }
+        else
+        {
+            //Debug.Log("Auto Aim Off");
+            cooldownIdikator = false;
+        }
+    }
+
+    public void moreDamageButton()
+    {
+        if (moreDamage == false)
+        {
+            playerAttackHitbox.gameObject.GetComponent<PlayerAttackHitbox>().setDamage(moreDamage, addedDamage);
+            moreDamage = true;
+        }
+        else
+        {
+            playerAttackHitbox.gameObject.GetComponent<PlayerAttackHitbox>().setDamage(moreDamage, addedDamage);
+            moreDamage = false;
+        }
+    }
+}
