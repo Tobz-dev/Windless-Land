65010a9a1bdc6c84bd37a5c621f8692308fd33d6
AngryBread-git <67788270+AngryBread-git@users.noreply.github.com>
2022-01-03 18:22:06 +0100

    created ManaFountainV2. which is interactable, and can only be used once.
    haven't placed V2 in the levels yet.

--- Assets/LukasKleberg/Scripts/ManaFountain.cs
+++ Assets/LukasKleberg/Scripts/ManaFountain.cs
@@ -7,19 +7,66 @@ public class ManaFountain : MonoBehaviour
     [SerializeField]
     private int manaRestoreValue;
 
-    private void OnTriggerEnter(Collider collision)
+    private PlayerInputs inputActions;
+
+    [SerializeField] private GameObject PressE;
+
+    private bool canPress, fountainFull = true;
+
+    private void Awake()
     {
-        if (collision.gameObject.tag == "Player")
+        inputActions = InputManager.inputActions;
+    }
+
+    private void Update()
+    {
+        if ((inputActions.WindlessLand.Interact.triggered || Input.GetKeyDown(KeyCode.E)))
         {
-            Debug.Log("ManaFountain. hit player");
-            if (collision.gameObject.GetComponent<CharacterController>().enabled)
+            if (fountainFull) 
             {
-                collision.gameObject.GetComponent<CharacterController>().ManaIncreased(manaRestoreValue);
+                Debug.Log("ManaFountain. interact key pressed");
+
+                EmptyFountain();
+
+                GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>().ManaIncreased(manaRestoreValue);
             }
-            else
+        }
+    }
+
+    private void EmptyFountain()
+    {
+        fountainFull = false;
+        //change mats of fountain parts here.
+        PressE.SetActive(false);
+    }
+
+    public void RestoreFountain() 
+    {
+        fountainFull = true;
+        //change mats of fountain parts here.
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canPress = true;
+
+            if (fountainFull)
             {
-                collision.gameObject.GetComponent<CharacterControllerRemapTest>().ManaIncreased(manaRestoreValue);
+                PressE.SetActive(true);
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            canPress = false;
+            //panel.SetActive(false);
+            PressE.SetActive(false);
         }
     }
 }
