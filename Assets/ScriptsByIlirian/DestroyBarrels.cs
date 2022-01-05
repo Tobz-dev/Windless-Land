8a8f51428935049bab9d7a298c8c1ef00195d02e
IlirianZuta <82038180+IlirianZuta@users.noreply.github.com>
2022-01-05 19:11:38 +0100

    Update DestroyBarrels.cs

--- Assets/ScriptsByIlirian/DestroyBarrels.cs
+++ Assets/ScriptsByIlirian/DestroyBarrels.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class DestroyBarrels : MonoBehaviour
 {
     [SerializeField] private GameObject destroyedBarrel;
-    public float bforce = 1f;
-    protected Rigidbody rb;
-    private int active = 0;
+
 
     
     [SerializeField] private GameObject sword;
