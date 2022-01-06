d988aeabe14447a1135b1822f74aa8526c14dae5
ra1l0 <agelii.tim@gmail.com>
2022-01-06 20:09:32 +0100

    Update OptionsButtonHandler.cs

--- Assets/LukasKleberg/MainMenu/MenuScripts/OptionsButtonHandler.cs
+++ Assets/LukasKleberg/MainMenu/MenuScripts/OptionsButtonHandler.cs
@@ -7,21 +7,6 @@ public class OptionsButtonHandler : MonoBehaviour
 {
     public Image checkBoxImage;
 
-    private void Start()
-    {
-        if (PlayerPrefs.HasKey("inputDelay"))
-        {
-            if (PlayerPrefs.GetInt("inputDelay") == 1)
-            {
-                checkBoxImage.enabled = true;
-            }
-            if (PlayerPrefs.GetInt("inputDelay") == 0)
-            {
-                checkBoxImage.enabled = true;
-            }
-        }
-    }
-
     public void OnButtonPress()
     {
         //makes the checkbox active if it was inactive, and vice versa
