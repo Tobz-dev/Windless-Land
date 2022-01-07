cede7b700d90fa1311a448e09a5154a76c5557b9
SOTGGON <maoxiehui@outlook.com>
2022-01-07 20:52:17 +0100

    change

--- Assets/Source Material/Xiehui Mao/Scripts/WarningControl.cs
+++ Assets/Source Material/Xiehui Mao/Scripts/WarningControl.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class WarningControl : MonoBehaviour
 {
     public GameObject warning;
-    public AudioSource danger;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             warning.SetActive(true);
-            danger.Play();
         }
     }
 
