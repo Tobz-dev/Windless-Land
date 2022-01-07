68fda27de7ad1ec91004e93f17000e51c8610cc4
IndustrialTV2 <johan.nydahl@hotmail.se>
2022-01-07 19:43:56 +0100

    Cleanup

--- Assets/Johan/VFXPrototype2.cs
+++ Assets/Johan/VFXPrototype2.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//Main Author: Johan Nydahl
 
 public class VFXPrototype2 : MonoBehaviour
 {
--- Assets/Johan/VFX_TrapArrow/TrapArrowForward.cs
+++ Assets/Johan/VFX_TrapArrow/TrapArrowForward.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Main Author: Johan Nydahl
 
 public class TrapArrowForward : MonoBehaviour
 {
