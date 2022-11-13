20ca5c276636b8291aabd6c433eb32cefa1ebc94
AngryBread-git <67788270+AngryBread-git@users.noreply.github.com>
2022-11-13 16:14:03 +0100

    Canvas v5.0 added in scenes.
    in all active scenes.

--- Assets/Source Material/Miranda Greting/Scripts/PlayerUI.cs
+++ Assets/Source Material/Miranda Greting/Scripts/PlayerUI.cs
@@ -37,6 +37,8 @@ public class PlayerUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+
         hpScript = player.GetComponent<PlayerHealthScript>();
 
         maxHealth = (int)hpScript.GetMaxHealth();
